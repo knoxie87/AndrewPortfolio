14585e6b23b5e874a1a50a7c4568b41d43f45e06
Andrew <andrew.knox87@gmail.com>
2023-02-03 13:02:07 +1300

    removed unnecessary components

--- src/Layout/Overlay.js
+++
@@ -1,9 +0,0 @@
-const Overlay = (props) => {
-    return (
-        <div style={{width: '100%',height: '100%', position:'fixed', top: 0, left: 0, backgroundColor:'rgb(0,0,0,0.25)', zIndex:-20}}>
-
-        </div>
-    )
-}
-
-export default Overlay
